005ce9b988a3559a8899cf9cbf9f9cdb261589ab
Brittany <brterbush@zappos.com>
2021-04-27 23:33:12 -0700

    Use fetch instead of request.

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -5,30 +5,32 @@ import Typography from "@material-ui/core/Typography"
 import { Button, Paper } from "@material-ui/core"
 import StockList from "../StockList/StockList"
 const API_KEY = `${process.env.REACT_APP_STOCK_API_KEY}`
-const request = require("request")
-
 class App extends React.Component {
     constructor(props) {
         super(props)
 
         this.state = {
-            stockResults: []
+            isLoading: true,
+            stocks: [],
+            error: null
         }
 
         this.fetchStocks = this.fetchStocks.bind(this)
     }
 
     fetchStocks() {
-        request(
-            `https://finnhub.io/api/v1/stock/symbol?exchange=US&token=${API_KEY}`,
-            { json: true },
-            (err, res, body) => {
-                if (err) {
-                    return console.log(err)
-                }
-                console.log(res.body)
-            }
+        fetch(
+            `https://api.polygon.io/v2/reference/tickers?sort=ticker&perpage=50&page=1&apiKey=${API_KEY}`
         )
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data.tickers)
+                this.setState({
+                    stocks: data.tickers,
+                    isLoading: false
+                })
+            })
+            .catch((error) => this.setState({ error, isLoading: false }))
     }
 
     render() {
